7b75fa69ca615cfca866064ac4e312607b9b7a01
Connor Streng <connor.streng@mediamonks.com>
2022-04-27 09:37:51 +0200

    Create Project

---
+++ src/components/atoms/Heading/Heading.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta } from '@storybook/react';
+
+import { createTextTemplate } from '../../../storybook-helpers/stories';
+import Heading, { HeadingProps } from './Heading';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as Array<HeadingProps['type']>,
+      },
+    },
+    text: { control: 'text' },
+  },
+} as Meta;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Template = createTextTemplate(Heading);
+
+export const Default = Template.bind({});
+Default.args = { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' };
---
+++ src/components/atoms/Paragraph/Paragraph.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta } from '@storybook/react';
+
+import { createTextTemplate } from '../../../storybook-helpers/stories';
+import Paragraph, { ParagraphProps } from './Paragraph';
+
+export default {
+  title: 'Paragraph',
+  component: Paragraph,
+  argTypes: {
+    size: {
+      control: { type: 'select', options: ['medium', 'small'] as Array<ParagraphProps['size']> },
+    },
+    weight: {
+      control: { type: 'select', options: ['regular', 'bold'] as Array<ParagraphProps['weight']> },
+    },
+    text: { control: 'text' },
+  },
+} as Meta;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Template = createTextTemplate(Paragraph);
+
+export const Default = Template.bind({});
+Default.args = { text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' };
